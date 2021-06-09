98c5456987974eae32213d9a959c05998914125e
Sanjay-Kurian <sanjayvkurian@gmail.com>
2021-06-09 12:25:40 +0530

    Added:Coin flipped multiple times and percentage of occurance is displayed

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -x
-result=$((RANDOM%2))
-if [ $result -eq 0 ]
-then
-	echo Heads won
-else
-	echo Tails won
-fi
+declare -A singlet
+heads=0
+tails=0
+for((i=0;i<5;i++))
+do
+	result=$((RANDOM%2))
+	if [ $result -eq 0 ]
+	then
+		echo Heads won
+		((heads++))
+	else
+		echo Tails won
+		((tails++))
+	fi
+done
+hperc=$((($heads*100)/5))
+tperc=$((($tails*100)/5))
+singlet[Heads]=$hperc
+singlet[Tails]=$tperc
+echo "The Singlet combinations are" ${!singlet[@]}
+echo "The Percentage of each combination occuring are" ${singlet[@]}
